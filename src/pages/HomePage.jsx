95d8a5ab0cceb1cf7db0437b7b8181a9d47e9140
Michael Cordero <magcordero@ucdavis.edu>
2025-01-17 13:02:41 -0800

    create mockup for landing page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,24 @@
 const HomePage = () => {
     return (
-        <div className="homepage">Homepage</div>
+        <div className="homepage-container">
+            <div className="homepage-title unselectable">HXNS</div>
+            <div className="homepage-collage-container">
+                <div className="homepage-center-piece"/>
+                <div className="homepage-photo-1">1</div>
+                <div className="homepage-photo-2">2</div>
+                <div className="homepage-photo-3">3</div>
+                <div className="homepage-photo-4">4</div>
+                <div className="homepage-photo-5">5</div>
+                <div className="homepage-photo-6">6</div>
+                <div className="homepage-photo-7">7</div>
+                <div className="homepage-photo-8">8</div>
+                <div className="homepage-photo-9">9</div>
+                <div className="homepage-photo-10">10</div>
+                <div className="homepage-photo-11">11</div>
+                <div className="homepage-photo-12">12</div>
+                <div className="homepage-photo-13">13</div>
+            </div>
+        </div>
     )
 }
 
