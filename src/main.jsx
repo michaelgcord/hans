79274cb38b81cfa291a07cf538031a688793aacd
Michael Cordero <magcordero@ucdavis.edu>
2025-01-16 18:22:05 -0800

    setup react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import HomePage from './pages/HomePage.jsx'
+import Timeline from './pages/Timeline.jsx'
+import './assets/css/index.css'
+
+const router = createBrowserRouter([
+	{
+		path: '/hans/',
+		element: <HomePage />
+	},
+	{
+		path: '/hans/timeline',
+		element: <Timeline />
+	}
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<RouterProvider router={router}/>
+	</StrictMode>,
 )
